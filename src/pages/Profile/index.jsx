89e5c8f5ba7113300ca0d6309fb986eb0d1fbed3
ja1antonio <ja1.negocios77@gmail.com>
2022-11-29 08:57:31 -0400

    Adicionar avatar ao nossos users

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -17,6 +17,9 @@ export function Profile() {
   const [passwordOld, setPasswordOld] = useState();
   const [passwordNew, setPasswordNew] = useState();
 
+  const [avatar, setAvatar] = useState(user.avatar);
+  const [avatarFile, setAvatarFile] = useState(null);
+
   async function handleUpdate() {
     const user = {
       name,
@@ -25,7 +28,15 @@ export function Profile() {
       old_password: passwordOld,
     };
 
-    await updateProfile({ user });
+    await updateProfile({ user, avatarFile });
+  }
+
+  function handleChangeAvatar(event) {
+    const file = event.target.files[0];
+    setAvatarFile(file);
+
+    const imagePreview = URL.createObjectURL(file);
+    setAvatar(imagePreview);
   }
 
   return (
@@ -38,12 +49,12 @@ export function Profile() {
 
       <Form>
         <Avatar>
-          <img src="https://github.com/ja1antonio.png" alt="Foto do usuário" />
+          <img src={avatar} alt="Foto do usuário" />
 
           <label htmlFor="avatar">
             <FiCamera />
 
-            <input id="avatar" type="file" />
+            <input id="avatar" type="file" onChange={handleChangeAvatar} />
           </label>
         </Avatar>
 
