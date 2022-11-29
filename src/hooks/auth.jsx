4deaf89322b5965acdf10924bdd4d3a58bd0246b
ja1antonio <ja1.negocios77@gmail.com>
2022-11-29 08:41:03 -0400

    Perfil sendo atualizado, nome, email e senha nova.

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -33,6 +33,22 @@ function AuthProvider({ children }) {
     setData({});
   }
 
+  async function updateProfile({ user }) {
+    try {
+      await api.put('/users', user);
+      localStorage.setItem('@rocketnotes:user', JSON.stringify(user));
+
+      setData({ user, token: data.token });
+      alert('Perfil atualizado!');
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert('Não foi possível atualizar o perfil.');
+      }
+    }
+  }
+
   useEffect(() => {
     const token = localStorage.getItem('@rocketnotes:token');
     const user = localStorage.getItem('@rocketnotes:user');
@@ -52,6 +68,7 @@ function AuthProvider({ children }) {
       value={{
         signIn,
         signOut,
+        updateProfile,
         user: data.user,
       }}
     >
