5dff3c4a30a7ae4e03f4e989d4c0cdd411caa041
ja1antonio <ja1.negocios77@gmail.com>
2022-12-07 08:21:47 -0400

    Adicionando link na nota e removendo

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -8,8 +8,21 @@ import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 
 import { Container, Form } from './styles';
+import { useState } from 'react';
 
 export function New() {
+  const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState('');
+
+  function handleAddLink() {
+    setLinks((prevState) => [...prevState, newLink]);
+    setNewLink('');
+  }
+
+  function handleRemoveLink(deleted) {
+    setLinks((prevState) => prevState.filter((link) => link !== deleted));
+  }
+
   return (
     <Container>
       <Header />
@@ -25,8 +38,20 @@ export function New() {
           <Textarea placeholder="Observações" />
 
           <Section title="Links úteis">
-            <NoteItem value="https://rocketseat.com.br" />
-            <NoteItem isNew placeholder="Novo link" />
+            {links.map((link, index) => (
+              <NoteItem
+                key={String(index)}
+                value={link}
+                onClick={() => handleRemoveLink(link)}
+              />
+            ))}
+            <NoteItem
+              isNew
+              placeholder="Novo link"
+              value={newLink}
+              onChange={(e) => setNewLink(e.target.value)}
+              onClick={handleAddLink}
+            />
           </Section>
 
           <Section title="Marcadores">
