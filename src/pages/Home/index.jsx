77593b86555bacc215b4e23c0d5eb92542ea16f1
ja1antonio <ja1.negocios77@gmail.com>
2022-12-13 09:17:52 -0400

    Abertura de notas pelo home feito

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { FiPlus } from 'react-icons/fi';
 import { api } from '../../services/api';
@@ -17,10 +18,11 @@ export function Home() {
   const [tagsSelected, setTagsSelected] = useState([]);
   const [notes, setNotes] = useState([]);
 
+  const navigate = useNavigate();
+
   function handleTagSelected(tagName) {
     if (tagName === 'all') {
       return setTagsSelected([]);
-    } else {
     }
 
     const alreadySelected = tagsSelected.includes(tagName);
@@ -31,6 +33,10 @@ export function Home() {
     } else {
       setTagsSelected((prevState) => [...prevState, tagName]);
     }
+  }
+
+  function handleDetails(id) {
+    navigate(`/details/${id}`);
   }
 
   useEffect(() => {
@@ -89,7 +95,11 @@ export function Home() {
       <Content>
         <Section title="Minhas Notas">
           {notes.map((note) => (
-            <Note key={String(note.id)} data={note} />
+            <Note
+              key={String(note.id)}
+              data={note}
+              onClick={() => handleDetails(note.id)}
+            />
           ))}
         </Section>
       </Content>
